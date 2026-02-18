3b11bf2a65d2ceec577c0389f7ad144fb061a9b8
RobertSichombaBob <sichombarobertbob@gmail.com>
2026-02-18 18:12:00 +0200

    Properly register custom template filters and add error pages

--- portfolio/templatetags/custom_filters.py
+++ portfolio/templatetags/custom_filters.py
@@ -3,5 +3,11 @@ from django import template
 register = template.Library()
 
 @register.filter
-def split(value, delimiter=','):
-    return value.split(delimiter) if value else []
+def get_item(dictionary, key):
+    """Usage: {{ mydict|get_item:key }}"""
+    return dictionary.get(key)
+
+@register.filter
+def split(value, separator):
+    """Usage: {{ 'a,b,c'|split:',' }}"""
+    return value.split(separator)
